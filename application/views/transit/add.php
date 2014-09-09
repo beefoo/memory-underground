17e18cfd27ec9a77fca62cda5f9f1c3b8c783420
Brian Foo <brian@youaremyjoy.org>
2014-09-09 01:15:28 -0400

    first pass at transit add styles

--- application/views/transit/add.php
+++ application/views/transit/add.php
@@ -1,14 +1,14 @@
 <div id="transit-add" class="form-wrapper">
   
   <div class="form-tab-links">
-    <a href="#people" class="tab-link">People <small>(Trains)</small></a>
+    <a href="#people" class="tab-link active">People <small>(Trains)</small></a>
     <a href="#memories" class="tab-link">Memories <small>(Stations)</small></a>
     <a href="#view" class="tab-link">View Result</a>
   </div>
   
   <div class="form-tabs">
     
-    <div id="people" class="form-tab">      
+    <div id="people" class="tab active">      
       <label for="person-name">Type Names of People You Had Memories With<br />
         <small>For an optimal map, add between 5 and 20 people with at least 2 memories per person</small>
       </label>
@@ -19,7 +19,7 @@
       <ul id="people-list" class="tab-list"></ul>
     </div>
     
-    <div id="add-memory" class="form-tab">
+    <div id="add-memory" class="tab">
       <form id="memory-form">
         <label for="title">Memory Label<br /><small>Keep it short, this will be the name of a train station</small></label>
         <input type="text" name="title" placeholder="e.g. 21st birthday, Winter camping, Mexico Honeymoon" maxlength="40" />
@@ -29,11 +29,11 @@
           <input type="text" name="person-name" placeholder="Another name" />
           <a href="#add-person" class="person-add-link">Add Person</a>
         </div>
-        <button type="submit">Submit Memory</button>
+        <button type="submit" class="expand">Submit Memory</button>
       </form>
     </div>
     
-    <div id="memories" class="form-tab">
+    <div id="memories" class="tab">
       <a href="#add-memory" class="button tab-link">Add A Memory</a>
       <ul id="memories-list" class="tab-list"></ul>
     </div>
--- assets/js/views/transit.add.js
+++ assets/js/views/transit.add.js
@@ -1,9 +1,26 @@
 app.views.TransitAddView = Backbone.View.extend({
 
   el: '#transit-add',
+  
+  events: {
+    "click .tab-link"   : "showTab"
+  },
 
   initialize: function(options) {
     
+  },
+  
+  showTab: function(e){
+    e.preventDefault();
+    
+    var $link = $(e.currentTarget),
+        href = $link.attr('href'),
+        $tab = $(href),
+        $tabLink = $('.tab-link[href="'+href+'"]');
+        
+    $('.tab, .tab-link').removeClass('active');
+    $tab.addClass('active');
+    $tabLink.addClass('active');
   }
 
 });
