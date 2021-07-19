96a8e8b40dd03d3a47e540d52955ff475f2e4ce4
beefoo <brian@youaremyjoy.org>
2021-07-19 13:36:47 -0400

    add simple server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-// A simple web server
-// > npm install connect serve-static
 // > node server.js 8888
-var port = 8080;
+var port = 2222;
 if (process.argv.length > 2) port = parseInt(process.argv[2]);
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(port, function(){
-  console.log('Server running on '+port+'...');
-});
+
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./')); //Tells the app to serve static files from ./
+
+app.listen(port, () => console.log('Listening on port '+port));
